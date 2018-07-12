4006ace1f38a6f13c94c47a6f40a42deee1ebb97
Artem Tarasov <artemtarsov89@gmail.com>
2018-07-12 13:58:05 -0400

    update

--- json_parse.py
+++ json_parse.py
@@ -10,7 +10,7 @@ directory_path = os.getcwd()
 path = os.path.join(directory_path, 'wading-pools.json')
 
 
-with open(path, 'r') as f:
+with open(path, 'r', encoding="UTF-8") as f:
     json_data = json.load(f)
 
 
@@ -24,14 +24,14 @@ for p in pools_list:
     pool = p['properties']
     pool_location = p['geometry']
     pool_coord = pool_location['coordinates']
-    pool_coord = [angle(pool_coord[0]), angle(pool_coord[1])] # remove if needed values in degree
+    # pool_coord = [angle(pool_coord[0]), angle(pool_coord[1])] # remove if needed values in degree
     pools_list_refined.append({'name': pool['NAME'],
                                'coordinates': pool_coord})
 
-print('unsorted: ' , pools_list_refined)
+print('unsorted: ', pools_list_refined)
 
 #sorting by coordinates, second is longitude, the most western is smallest(I think)
-pools_list_refined.sort(key=lambda pool: pool['coordinates'][1], reverse=False)
+#pools_list_refined.sort(key=lambda pool: pool['coordinates'][0], reverse=False)
 
 
 print('sorted: ', pools_list_refined)
@@ -41,11 +41,10 @@ data_frame = pd.DataFrame(pools_list_refined)
 
 csv_out = os.path.join(directory_path, 'csv.xls')
 data_frame.to_csv(csv_out, encoding='utf-8', index=False)
-
 #output
-path_out = os.path.join(directory_path, 'output.pl')
-with open(path_out, 'w') as f:
+path_out = os.path.join(directory_path, 'a.txt')
+with open(path_out, 'w', encoding='utf-8') as f:
     for e in pools_list_refined:
-        to_append = 'pool({0}, coordinates({1}, {2})).\n'.format(e['name'].split(' - ')[1], e['coordinates'][0], e['coordinates'][1])
+        to_append = '{0},{1},{2}\n'.format(e['name'].split(' - ')[1].lower(), e['coordinates'][1], e['coordinates'][0])
         f.write(to_append)
 
---
+++ sql/test.pyw
@@ -0,0 +1,10 @@
+#!C:\Users\Why am I here\AppData\Local\Programs\Python\Python36\pythonw.exe
+
+
+
+def main():
+    return "hi"
+
+
+if __name__ == "__main__":
+    main()
